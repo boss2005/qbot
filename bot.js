9dd1a0a9baab9a020e76f8ccb9885abaa24c7d43
boss2005 <47650848+boss2005@users.noreply.github.com>
2019-02-20 02:30:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3029,6 +3029,36 @@ client.on('message', message => {
 
 
 
+const discord_token = "nNTQ2Mjc5Nzc3NTk0NjM4MzQ0.D04wag.ZcxCvTAXs4v7Y6Lcv79mNKTo848";
+const prefix = "$"
+client.login(discord_token);
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '232564113459511306') return;
+
+if (message.content.startsWith(prefix + 'g')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 'w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(prefix + 'l')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 's')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+
+});
+
 
 //MHSTR END NOW THIS IS END
 client.login(process.env.BOT_TOKEN);
